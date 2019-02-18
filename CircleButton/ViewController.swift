504bf3465900b1d5bc6e844541191d5c3d1623ff
zhouqitian <zhouqitian@sansi.com>
2019-02-18 08:58:22 +0800

    fix demo

--- CircleButton/ViewController.swift
+++ CircleButton/ViewController.swift
@@ -12,16 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         
         let btn = DirectionButtonGroup.init(frame: CGRect.init(x: 100, y: 100, width: 200, height: 200))
         
         view.addSubview(btn)
-    }
-    
-    @objc func testClick() {
-        print("button clicked!!!")
     }
 
 
